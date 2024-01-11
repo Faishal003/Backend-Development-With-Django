a73fbef5da1c449c714dc3e071ff4891b8017ea7
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-01-12 05:34:18 +0600

    models(more examples)

--- first_app/models.py
+++ first_app/models.py
@@ -14,7 +14,15 @@ class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
-    num_stars = models.IntegerField()
+
+    rating = (
+        (1, "Worst"),
+        (2, "Bad"),
+        (3, "Not Bad"),
+        (4, "Good"),
+        (5, "Excellent!")
+    )
+    num_stars = models.IntegerField(choices=rating)
 
     def __str__(self):
         return self.name + " " + str(self.num_stars)
