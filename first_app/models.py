5cc08ef08379b43db4054c4130f1cccc47bb878e
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-01-12 04:06:24 +0600

    model objects

--- first_app/models.py
+++ first_app/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Musian(models.Model):
+class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Album(models.Model):
-    artist = models.ForeignKey(Musian, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_stars = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " " + str(self.num_stars)
