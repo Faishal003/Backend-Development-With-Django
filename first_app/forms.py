acfd3c60ee98c17490649158a6a374dcf4c2fbb1
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-01-30 04:36:26 +0600

    Form Submission

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class user_form(forms.Form):
-    user_name = forms.CharField(label='User-Name', max_length=100, widget=forms.TextInput(attrs={'placeholder':'Enter your name', 'style':'width:300px'}))
+    user_name = forms.CharField(label='User Name', max_length=100, widget=forms.TextInput(attrs={'placeholder':'Enter your name', 'style':'width:300px'}))
     user_dob = forms.DateField(label = 'Date of Birth', widget=forms.TextInput(attrs={'type':'date'}))
     user_email = forms.EmailField(label='Email', max_length=100)
--- first_app/views.py
+++ first_app/views.py
@@ -12,4 +12,17 @@ def index(request):
 def from_view(request):
     new_form = forms.user_form()
     diction = {'test_form':new_form, 'heading_1':"This from is creating usign django library"}
+
+    if request.method == 'POST':
+        new_form = forms.user_form(request.POST)
+
+        if new_form.is_valid():
+            user_name = new_form.cleaned_data['user_name']
+            user_dob = new_form.cleaned_data['user_dob']
+            user_email = new_form.cleaned_data['user_email']
+
+            diction.update({'user_name':user_name})
+            diction.update({'user_dob':user_dob})
+            diction.update({'user_email':user_email})
+            diction.update({'form_submitted': 'yes'})
     return render(request, 'first_app/form.html', context=diction)
