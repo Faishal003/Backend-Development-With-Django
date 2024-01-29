243aace3366dece0120f6ca5748562659e23d1db
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-01-30 03:33:14 +0600

    Creating a Django Form 2

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class user_form(forms.Form):
-    user_name = forms.CharField(label='User Name', max_length=100)
+    user_name = forms.CharField(label='User-Name', max_length=100, initial='Enter your name')
     user_email = forms.EmailField(label='Email', max_length=100)
