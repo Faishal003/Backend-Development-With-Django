c013889d585f8c8e7295b60ee75ed7f9f9cad51b
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-02 19:36:34 +0600

    Template Inheritance

--- first_app/urls.py
+++ first_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from first_app import views
 from django.contrib import admin
 
+app_name = 'first_app'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('form/', views.from_view, name='form'),
