43daa38c0dc7e623a10928067e662324ab740f85
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-02 19:02:33 +0600

    Django Model Forms

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,19 +1,9 @@
 from django import forms
 from django.core import validators
+from first_app.models import Album, Musician
 
-# def even_validator(value):
-#     if value % 2 != 0:
-#         raise forms.ValidationError('Number is not even')
-
-class user_form(forms.Form):
-    # number_filed = forms.IntegerField(validators=[even_validator])
-    user_email = forms.EmailField()
-    user_vmail = forms.EmailField()
-
-    def clean(self):
-        all_clean_data = super().clean()
-        user_email = all_clean_data['user_email']
-        user_vmail = all_clean_data['user_vmail']
-
-        if user_email != user_vmail:
-            raise forms.ValidationError('Make sure emails match')
+class MusicianForm(forms.ModelForm):
+    class Meta:
+        model = Musician
+        exclude = ["last_name"]
+        # include = ["first_name", "instrument"]
--- first_app/views.py
+++ first_app/views.py
@@ -10,14 +10,14 @@ def index(request):
     return render(request, 'first_app/index.html', context=diction)
 
 def from_view(request):
-    new_form = forms.user_form()
-    diction = {'test_form':new_form, 'heading_1':"This from is creating usign django library"}
+    new_form = forms.MusicianForm()
 
     if request.method == 'POST':
-        new_form = forms.user_form(request.POST)
-        diction.update({'test_form':new_form})
+        new_form = forms.MusicianForm(request.POST)
 
         if new_form.is_valid():
-            diction.update({'number_filed': 'Fields are matched.'})
-            diction.update({'form_submitted': 'yes'})
+            new_form.save(commit=True)
+            return index(request)
+
+    diction = {'test_form':new_form, 'heading':'Add Musician'}
     return render(request, 'first_app/form.html', context=diction)
