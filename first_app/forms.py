b412acac4aa647996819c16bfe00edbb02d14ae0
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-07 20:34:48 +0600

    Entry Forms

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from django.core import validators
-from first_app.models import Album, Musician
+from first_app import models
 
 class MusicianForm(forms.ModelForm):
     class Meta:
-        model = Musician
-        exclude = ["last_name"]
-        # include = ["first_name", "instrument"]
+        model = models.Musician
+        fields = '__all__'
+
+class AlbumForm(forms.ModelForm):
+    #model form ke override kortesi nijer iccha moto
+    release_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+    class Meta:
+        model = models.Album
+        fields = '__all__'
--- first_app/views.py
+++ first_app/views.py
@@ -5,7 +5,8 @@ from first_app import forms
 # Create your views here.
 
 def index(request):
-    diction = {'title': "Home Page"}
+    musicain_list = Musician.objects.order_by('first_name')
+    diction = {'title': "Home Page", 'musicain_list': musicain_list}
     return render(request, 'first_app/index.html', context=diction)
 
 def album_list(request):
@@ -13,9 +14,26 @@ def album_list(request):
     return render(request, 'first_app/album_list.html', context=diction)
 
 def musician_form(request):
-    diction = {'title': "Musician Form"}
+    form = forms.MusicianForm()
+
+    if request.method == 'POST':
+        form = forms.MusicianForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+    diction = {'title': "Musician Form", 'musician_form': form}
     return render(request, 'first_app/musician_form.html', context=diction)
 
 def album_form(request):
-    diction = {'title': "Album Form"}
+    form = forms.AlbumForm()
+
+    if request.method == 'POST':
+        form = forms.AlbumForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+    diction = {'title': "Album Form", 'album_form': form}
     return render(request, 'first_app/album_form.html', context=diction)
