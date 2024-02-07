6b2bb77756299679032395d0cdaa257d767ce470
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-07 08:07:56 +0600

    Connecting MySql Database

--- Backend_Development_With_Django/settings.py
+++ Backend_Development_With_Django/settings.py
@@ -65,10 +65,14 @@ WSGI_APPLICATION = 'Backend_Development_With_Django.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'first_database',
+		'USER': 'root',
+		'PASSWORD': '',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
 
 
