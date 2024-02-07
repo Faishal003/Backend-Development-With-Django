a661f3dc4b14c1ef890a7ab330be639da259b02b
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-07 23:49:54 +0600

    Dynamic Template wirte and add album list

--- first_app/views.py
+++ first_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Musician, Album
 from first_app import forms
+from django.db.models import Avg
 # Create your views here.
 
 def index(request):
@@ -12,7 +13,9 @@ def index(request):
 def album_list(request, artist_id):
     artist_info = Musician.objects.get(pk=artist_id)
     album_list = Album.objects.filter(artist=artist_id).order_by('name')
-    diction = {'title': "Album List", 'artist_info': artist_info, 'album_list': album_list}
+    artist_rating = Album.objects.filter(artist=artist_id).aggregate(Avg('num_stars'))
+
+    diction = {'title': "Album List", 'artist_info': artist_info, 'album_list': album_list, 'artist_rating': artist_rating}
     return render(request, 'first_app/album_list.html', context=diction)
 
 def musician_form(request):
