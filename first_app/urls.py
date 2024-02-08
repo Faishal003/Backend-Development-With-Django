0f0ea162c2a165e4e602520ba4a7922d3a36c0b7
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-09 04:32:40 +0600

    Update Data Part 3 (album add successfully)

--- first_app/urls.py
+++ first_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('musician_form/', views.musician_form, name='musician_form'),
     path('album_form/', views.album_form, name='album_form'),
     path('edit_artist/<int:artist_id>/', views.edit_artist, name='edit_artist'),
+    path('edit_album/<int:album_id>/', views.edit_album, name='edit_album'),
 
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -53,6 +53,22 @@ def edit_artist(request, artist_id):
         if form.is_valid():
             form.save(commit=True)
             return album_list(request, artist_id)
-            
+
     diction = {'edit_form': form}
     return render(request, 'first_app/edit_artist.html', context=diction)
+
+def edit_album(request, album_id):
+    album_info = Album.objects.get(pk=album_id)
+    form = forms.AlbumForm(instance=album_info)
+    diction = {}
+
+    if request.method == 'POST':
+        form = forms.AlbumForm(request.POST, instance=album_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            diction.update({'success_text': "Album Updated!"})
+            # return album_list(request, album_info.artist.id)
+
+    diction.update({'edit_form': form})
+    return render(request, 'first_app/edit_album.html', context=diction)
