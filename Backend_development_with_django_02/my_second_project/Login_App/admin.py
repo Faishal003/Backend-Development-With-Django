f4c08f98cb222e72a4d891d686a2cebfb6cf0d69
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-11 23:23:22 +0600

    Using Built-in User Model

--- Backend_development_with_django_02/my_second_project/Login_App/admin.py
+++ Backend_development_with_django_02/my_second_project/Login_App/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from Login_App.models import UserInfo
 
 # Register your models here.
+admin.site.register(UserInfo)
--- Backend_development_with_django_02/my_second_project/my_second_project/settings.py
+++ Backend_development_with_django_02/my_second_project/my_second_project/settings.py
@@ -4,6 +4,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -107,6 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR,]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
