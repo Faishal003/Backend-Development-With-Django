05f9f7ebb67daeabeca9ba4378be9111a46ca59b
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-02-14 19:30:16 +0600

    Register User

--- Backend_development_with_django_02/my_second_project/Login_App/views.py
+++ Backend_development_with_django_02/my_second_project/Login_App/views.py
@@ -7,7 +7,28 @@ def index(request):
     return render(request, 'Login_App/index.html', context=dict)
 
 def register(request):
-    user_form = UserForm()
-    user_info_form = UserInfoForm()
-    dict = {'user_form': user_form, 'user_info_form': user_info_form}
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        user_info_form = UserInfoForm(data=request.POST)
+
+        if user_form.is_valid() and user_info_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            user_info = user_info_form.save(commit=False) # this info can not go to the database directly because commit is set to False
+            user_info.user = user
+            if 'profile_pic' in request.FILES:
+                user_info.profile_pic = request.FILES['profile_pic']
+
+            user_info.save()
+            registered = True
+    else:
+        user_form = UserForm()
+        user_info_form = UserInfoForm()
+
+    dict = {'user_form': user_form, 'user_info_form': user_info_form, 'registered': registered}
     return render(request, 'Login_App/register.html', context=dict)
