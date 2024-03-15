24a1a9adf6c33e26be0ffc0b6e76b1c7997ec5ae
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-15 15:51:48 +0600

    template literals

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+let pencilePrice = 10;
+let penPrice = 20;
+
+// console.log("Total price is", pencilePrice + penPrice, "Taka");
+let output = `Total price is ${pencilePrice + penPrice} Taka`;
+console.log(output);
