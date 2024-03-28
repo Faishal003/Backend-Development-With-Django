28a5652466a6b9c44ba523e69fc7aef14268d79d
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-28 11:51:37 +0600

    JS (nested forof Loops)

--- JS/app.js
+++ JS/app.js
@@ -8,6 +8,17 @@ for (let fruit of fruits) {
 
 //can be print string also
 
-for (char of 'gigabyte'){
+for (char of 'gigabyte') {
     console.log(char); //print out the individual characters
+}
+
+//nested forof loops
+
+let heros = [['batman', 'superman'], ['spiderman', 'ironman'], ['wonderwoman', 'captainamerica']];
+
+for (list of heros) {
+    console.log(`outer loop: #{${list}}`);
+    for (hero of list) {
+        console.log(hero);
+    }
 }
