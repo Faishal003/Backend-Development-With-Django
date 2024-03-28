0336a2042d2368c230d0b1cfc629d9bafa2e11e5
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-28 16:13:25 +0600

    JS (nested object literals)

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,25 @@
-const student = {
-    name: "Steve Smith",
-    age: 32,
-    content: "this is my first post @firstPost",
-    likes: 130,
-    repost: 30,
-    tags: ['@funnyPost', '@funnyCommunity'],
+//object literal
+
+const classInfo = {
+    faishal: {
+        grade: 'A+',
+        city: "rajshahi",
+    },
+
+    ahmed: {
+        grade: 'A',
+        city: "dhaka",
+    },
+
+    emon: {
+        grade: 'A-',
+        city: "natore",
+    }
 }
 
-console.log(student.name);
-console.log(student.content);
+console.log(classInfo.faishal.city)
 
-//now add a variable and access that variable with .
-let firstname = "name";
-console.log(student.firstname); //cannot access with dot
-console.log(student[firstname]); //now it should be accessed.
+//change value
+classInfo.faishal.city = 'barisal';
+console.log(classInfo.faishal.city);
+console.log(classInfo);
