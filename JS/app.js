9819c481997da9cf49d022c8e9c1fbb8bd6ef0dd
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-28 19:52:17 +0600

    JS (guessing game basic)

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,23 @@
-//object literal
+//guessing game
 
-const classInfo = {
-    faishal: {
-        grade: 'A+',
-        city: "rajshahi",
-    },
+const max = prompt("Tell the guessing game range");
 
-    ahmed: {
-        grade: 'A',
-        city: "dhaka",
-    },
+const random = Math.floor(Math.random() * max) + 1;
 
-    emon: {
-        grade: 'A-',
-        city: "natore",
+let guess = prompt(`guess any number between 1 to ${max}`);
+
+while (true) {
+    if (guess == 'exit') {
+        console.log("You just logged out of the game");
+        break;
+    }
+
+    if (guess == random) {
+        console.log("guess matched", random);
+        break;
+    } else if (guess < random) {
+        guess = prompt("hint: Your number is too small. Please try again");
+    } else {
+        guess = prompt("hint: Your number is too large. Please try again.")
     }
 }
-
-console.log(classInfo.faishal.city)
-
-//change value
-classInfo.faishal.city = 'barisal';
-console.log(classInfo.faishal.city);
-console.log(classInfo);
