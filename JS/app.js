17126fb013ddc473073b180ba3b273278b6a994d
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-28 14:57:59 +0600

    JS (create an object literal)

--- JS/app.js
+++ JS/app.js
@@ -1,31 +1,16 @@
-//todo list app
-//main functionality is list, add, delete, and quit
-
-
-let req = prompt("What would you like to do?");
-let todo = [];
-
-while (true) {
-    if (req == "quit") {
-        console.log("You quit the app");
-        break;
-    } else if (req == "list") {
-        console.log("**********");
-        for (let i = 0; i < todo.length; i++) {
-            console.log(i, todo[i]);
-        }
-        console.log("**********");
-    } else if (req == "add") {
-        let task = prompt("Enter a new task");
-        todo.push(task);
-        console.log("Task added");
-    } else if (req == "delete") {
-        let idx = prompt("Enter the index of the task to delete");
-        todo.splice(idx, 1);
-        console.log("Task deleted");
-    } else {
-        console.log("You entered a wrong input");
-    }
-
-    req = prompt("What would you like to do?");
+const student = {
+    name: "Steve Smith",
+    age: 32,
+    content: "this is my first post @firstPost",
+    likes: 130,
+    repost: 30,
+    tags: ['@funnyPost', '@funnyCommunity'],
 }
+
+console.log(student.name);
+console.log(student.content);
+
+//now add a variable and access that variable with .
+let firstname = "name";
+console.log(student.firstname); //cannot access with dot
+console.log(student[firstname]); //now it should be accessed.
