c681aac8fb192d3aed4e510dc057118844a414d1
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-28 10:38:47 +0600

    JS (nested loops)

--- JS/app.js
+++ JS/app.js
@@ -1,38 +1,7 @@
-//Strings Methods
-
-//Trim Method
-let str = "   Hello World   ";
-console.log(str.trim());
-let str1 = "   Hel  lo Wo  rld   ";//trim method only remove the spaces from the start and end of the string
-console.log(str1.trim());
-
-
-//uppercase and lowercase
-let str2 = "Hello World";
-console.log(str2.toUpperCase());
-console.log(str2.toLowerCase());
-
-//Indexof Method
-let msg = "ilovecoding";
-console.log(msg.indexOf("c"));
-
-//multiple methods
-let msg1 = "    Hello          ";
-console.log(msg1.trim().toUpperCase());
-
-//Slice Method
-let msg2 = "explanation";
-console.log(msg2.slice(4));
-console.log(msg2.slice(4, 11));
-console.log(msg2.slice(-1)); //length - 1 = 11 - 1 = 10
-console.log(msg2.slice(-4, -1)); //length - 4 = 11 - 4 = 7 and length - 1 = 11 - 1 = 10
-console.log(msg2.slice(7, 10)); //same as above
-
-//Replace Method
-let msg3 = "ilovecoding";
-console.log(msg3.replace("coding", "programming"));
-console.log(msg3.replace("i", "I")); //can not change the original string
-
-//Repeat Method
-let msg4 = "Hello";
-console.log(msg4.repeat(3));
+//nested loop
+for (i = 1; i <= 3; i++) {
+    console.log(`Outer loop ${i}`);
+    for (j =1; j <= 3; j++){
+        console.log(j);
+    }
+}
