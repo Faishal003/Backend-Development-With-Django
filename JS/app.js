d0a6bff5392cf63fc5359360e13c6c30a0dff2e5
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-28 12:49:40 +0600

    JS (todo basic app)

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,31 @@
-//forof loops
+//todo list app
+//main functionality is list, add, delete, and quit
 
-let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
 
-for (let fruit of fruits) {
-    console.log(fruit);
-}
+let req = prompt("What would you like to do?");
+let todo = [];
 
-//can be print string also
-
-for (char of 'gigabyte') {
-    console.log(char); //print out the individual characters
-}
-
-//nested forof loops
-
-let heros = [['batman', 'superman'], ['spiderman', 'ironman'], ['wonderwoman', 'captainamerica']];
-
-for (list of heros) {
-    console.log(`outer loop: #{${list}}`);
-    for (hero of list) {
-        console.log(hero);
+while (true) {
+    if (req == "quit") {
+        console.log("You quit the app");
+        break;
+    } else if (req == "list") {
+        console.log("**********");
+        for (let i = 0; i < todo.length; i++) {
+            console.log(i, todo[i]);
+        }
+        console.log("**********");
+    } else if (req == "add") {
+        let task = prompt("Enter a new task");
+        todo.push(task);
+        console.log("Task added");
+    } else if (req == "delete") {
+        let idx = prompt("Enter the index of the task to delete");
+        todo.splice(idx, 1);
+        console.log("Task deleted");
+    } else {
+        console.log("You entered a wrong input");
     }
+
+    req = prompt("What would you like to do?");
 }
