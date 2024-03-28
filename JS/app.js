afbb5df3a57f0cdba58550e29585ffca0a9444fe
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-03-28 11:14:33 +0600

    JS (practice of while loop with prompt basic)

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,12 @@
-//nested loop
-for (i = 1; i <= 3; i++) {
-    console.log(`Outer loop ${i}`);
-    for (j =1; j <= 3; j++){
-        console.log(j);
-    }
+//guess the movie
+
+let favourite = "The Shawshank Redemption";
+let guess = prompt("What is my favourite movie?");
+
+while ((guess != favourite) && (guess != "exit")) {
+    console.log("Wrong guess. Try again");
+    guess = prompt("What is my favourite movie?");
+}
+if (guess == favourite) {
+    console.log("Congrats! You guessed it right.");
 }
