4d4d4dc32866c155394608ac14bfa75ff60c5602
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-06 03:33:35 +0600

    JS (Practice of array functions)

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,24 @@
-//this with arrow function
+let arr = [1, 2, 3, 4, 5, 6]
 
-const student = {
-    name: 'faishal ahmed emon',
-    marks: 50,
-    prop: this, //global function
-
-    getName: function () {
-        console.log(this);
-        return this.name;
-    },
-
-    getMarks: () => {
-        console.log(this); //here this is  a parent scope not a calling object
-        return this.marks;
-    },
-
-    getInfo1: function () {
-        setTimeout(() => {
-            console.log(this);//student
-        }, 2000)
-    },
-
-    getInfo2: function (){
-        setTimeout(function() { //parent function is builtin function..
-            console.log(this);
-        }, 2000)
-    },
+const arrayAverage = (arr) => {
+    let result = 0;
+    for (i = 0; i < arr.length; i++) {
+        result = result + arr[i];
+    }
+    return result;
 }
+
+console.log(arrayAverage(arr));
+
+
+//second question
+
+const Even = (n) => {
+    if (n % 2 == 0) {
+        console.log("It's an even number.");
+    } else {
+        console.log("It's an odd number");
+    }
+}
+
+console.log(Even(6))
