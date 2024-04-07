9547ccd26c60d52710f7c55f8b7aa94f98b3ed39
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-08 05:40:41 +0600

    JS (array method)

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,58 @@
-let arr = [1, 2, 3, 4, 5, 6]
+//array method
 
-const arrayAverage = (arr) => {
-    let result = 0;
-    for (i = 0; i < arr.length; i++) {
-        result = result + arr[i];
+//map function
+
+let num = [1,2,3,4,5];
+
+let double = num.map((el)=>{
+    return(el*2);
+});
+
+let student = [
+    {
+    name: 'faishal ahmed emon',
+    marks: 45,
+}, 
+{
+    name: 'alex',
+    marks: 50,
+
+}, 
+{
+    name: 'xender',
+    marks: 49,
+},
+];
+
+let gpa = student.map((emon)=>{ //parameter should be any of these either its el or emon
+    return emon.marks / 10;
+})
+
+//filter function
+
+let number = [1,2,3,4,5,11,2,3,4,2,4,5,6,745,7,7,5,43,44,48];
+
+let evenOrOdd = number.filter((el)=>{
+    return el % 2 != 0; //use as many condition you want
+})
+
+//reduce method
+
+let nums = [1,2,3,4,5,6,7,8,9,10,10];
+
+let result = nums.reduce((res, el)=> {
+    console.log(res);
+    return res + el;
+})
+
+//maximum in array
+let  numbers = [1,4,3,2,1,4,6,7,5,4];
+
+let max = numbers.reduce((max, el)=>{
+    if(max > el){
+        return max;
     }
-    return result;
-}
-
-console.log(arrayAverage(arr));
-
-
-//second question
-
-const Even = (n) => {
-    if (n % 2 == 0) {
-        console.log("It's an even number.");
-    } else {
-        console.log("It's an odd number");
+    else{
+        return el;
     }
-}
-
-console.log(Even(6))
+})
