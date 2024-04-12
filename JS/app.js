2fca4ebfa9a3018cca33410b0e5ea6a62392f82c
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-12 12:43:52 +0600

    (adding element on the page)

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,35 @@
-let links = document.querySelectorAll(".box a")
-console.log(links)
+//adding elements on page
 
-for (link of links) {
-    link.style.color = 'green'; //inline style should change
-}
+//let create an element is paragraph
+let newP = document.createElement('p');
 
-// for (i = 0; i < links.length; i++){
-//     links[i].style.color = "yellow";
-// }
+//add some text inside the paragraph
+newP.intterText("This is a new paragraph section");
 
-//manipulating style\
-//classList.add() to add new class
-//classList.remove() to remove class
-//classList.contains() to check if class exists
-//classList.toggle() to toggle between add and remove
+//to check text add or not 
+newP //console command
+
+//now add the paragraph in any section i choose body section to do that
+let body = document.querySelector('body');
+
+//appendChild is need to add newap in body
+body.appendChild(newP) //text is added on the last in body element
+
+
+
+//now i want to add some text in newP section
+newP.append("extra added text");
+
+//also create some button
+let btn = document.createElement('button');
+
+//insted of using appendChild we can use append also
+body.append(btn) // added to the last.
+body.prepend(btn) //added to the first.
+
+//to add any position using insertAdjacentElement(which position should i put, which element should i put)
+
+body.insertAdjacentElement("beforebegin", btn);
+body.insertAdjacentElement("afterbegin",btn);
+body.insertAdjacentElement("beforeend",btn);
+body.insertAdjacentElement("afterend",btn);
