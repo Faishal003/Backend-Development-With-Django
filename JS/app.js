1a9c21e85a92773e5dae1edf15a99c15af0362f6
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-17 14:14:15 +0600

    (JS (practice of addeventlistener)

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,22 @@
-//addeventlistener
+let btn = document.querySelector('button')
 
-let p = document.querySelector("p")
-console.dir(p)
-p.addEventListener('click', function(){
-    p.style.color = "red"
-})
 
-let div = document.querySelector('div')
-div.addEventListener("mouseenter", function(){
-    div.style.backgroundColor = "yellow"
+function randomColor () {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+}
+
+btn.addEventListener('click', function() {
+    let h3 = document.querySelector('h3');
+    let randomColora = randomColor();
+    h3.innerText = randomColora;
+
+    let div = document.querySelector("div")
+    div.style.backgroundColor = randomColora;
+
+    console.log('color is updated')
 })
