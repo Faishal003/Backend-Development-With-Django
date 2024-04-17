aafeb5d03f8553cbcaceba453ed3f8a4ace0a2b7
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-17 15:22:33 +0600

    JS (Extracting from data)

--- JS/app.js
+++ JS/app.js
@@ -2,5 +2,14 @@ let form = document.querySelector("form")
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    alert("form is submitted");
+    // let user = document.querySelector("#user");
+    // let pass = document.querySelector("#pass");
+
+    let user = this.elements[0];//form.elements[0] both are same
+    let pass = this.elements[1];
+
+    console.log(user.value);
+    console.log(pass.value);
+
+    alert(`hi, ${user.value}! your submitted pass is ${pass.value}`);
 })
