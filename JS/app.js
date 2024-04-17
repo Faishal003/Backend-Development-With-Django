b375fffc6c20df566f5debb2e6178d566793fbfd
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-18 04:18:28 +0600

    JS (Todo list with dom using eventdelegation)

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,43 @@
-let div = document.querySelector("div");
-let ul = document.querySelector("ul");
-let lis = document.querySelectorAll("li");
+let btn = document.querySelector("button");
+let ul = document.querySelector('ul');
+let inp = document.querySelector('input');
 
-div.addEventListener("click", function(){
-    console.log("div was clicked");
+inp.addEventListener('keydown',function(event){
+    if(event.code == "Enter"){
+        btn.click();
+    }
 })
 
-ul.addEventListener("click", function(event){
-    event.stopPropagation();
-    console.log("ul was clicked");
+btn.addEventListener('click', function(){
+
+    let item = document.createElement('li');
+    let deleteBtn = document.createElement('button');
+
+    item.innerText = inp.value;
+    deleteBtn.innerText = "delete";
+    deleteBtn.classList.add("delete");
+
+    ul.appendChild(item);
+    item.appendChild(deleteBtn);
+
+    inp.value = "";
 })
 
-for(li of lis){
-    li.addEventListener("click", function(){
-        event.stopPropagation();
-        console.log("li was clicked");
-    })
-}
+ul.addEventListener('click', function(event){
+    // console.log(event.target.nodeName);
+    if(event.target.nodeName == 'BUTTON'){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+})
+
+//This should workd for existing list if add new value then it should not delete that value.
+// let delbtns = document.querySelectorAll('.delete');
+// for(delbtn of delbtns){
+//     delbtn.addEventListener('click', function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     })
+// }
+
