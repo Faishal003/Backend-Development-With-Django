6aa5acf2879533fdd88ee08db04d892a77b70b21
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-17 14:50:13 +0600

    (JS Form events)

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,6 @@
-let btn = document.querySelector('button')
+let form = document.querySelector("form")
 
-
-function randomColor () {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
-
-    let color = `rgb(${red},${green},${blue})`;
-    return color;
-}
-
-btn.addEventListener('click', function() {
-    let h3 = document.querySelector('h3');
-    let randomColora = randomColor();
-    h3.innerText = randomColora;
-
-    let div = document.querySelector("div")
-    div.style.backgroundColor = randomColora;
-
-    console.log('color is updated')
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("form is submitted");
 })
