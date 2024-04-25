91aeba59f4bad259738f97680ecc7b90c15b2f38
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-25 06:50:29 +0600

    JS (Simon Games Complete Code)

--- JS/app.js
+++ JS/app.js
@@ -6,6 +6,7 @@ let btnColor = ['pink','green','gray','purple'];
 let started = false;
 let level = 0;
 
+
 let h2 = document.querySelector('h2');
 
 document.addEventListener("keypress", function () {
@@ -25,23 +26,65 @@ function btnFlash(btn){
 }
 
 function levelUP() {
+    userSeq = [];
     level++;
     h2.innerText = `Level: ${level}`
     
     //random btn choose
     let randIdx = Math.floor(Math.random() * 4);
-    let btnIdx = btnColor[randIdx];
-    let randBtn = document.querySelector(`.${btnIdx}`)
+    let randColor = btnColor[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+
+    gameSeq.push(randColor);//game sequence color is added automatically on that.
+    console.log(`gameseq: ${gameSeq}`);
+
     btnFlash(randBtn);
 }
 
+function btnFlashUser(btn){
+    btn.classList.add("flash_black");
+    setTimeout(function(){
+        btn.classList.remove("flash_black")
+    }, 250)
+}
+
 function btnPress(){
-    //let btn = this;
-    console.log(this);
+    let btn = this;
+    btnFlashUser(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    console.log(`userseq: ${userSeq}`);
+
+    checkAns(userSeq.length - 1);
+}
+
+function checkAns(idx){
+
+    if (userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length === gameSeq.length){
+            setTimeout(levelUP, 1000);
+        }
+    }else{
+        h2.innerHTML = `Game over! Your Score is <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
 }
 
 let allBtns = document.querySelectorAll(".btn");
     for (btn of allBtns){
         btn.addEventListener("click", btnPress);
     }
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
 
