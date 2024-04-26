4f959b5994c917016993236f9c1cceba493d9c44
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-26 18:28:47 +0600

    JS (activity using query string ex: university list api)

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,32 @@
-//sending heades with api requests
+let url = "http://universities.hipolabs.com/search?name=";
 
-let url = "https://icanhazdadjoke.com/";
+let btn = document.querySelector("button");
 
-async function getJokes(){
+btn.addEventListener('click',async ()=>{
+    let country = document.querySelector("input").value;
 
-    try {
-        let config = {headers: {Accept: "application/json"}};
-        let res = await axios.get(url, config);
-        console.log(res.data);
-    } catch (error) {
-        console.log(`Error: ${error}`)
+    let uni = await getUniversities(country);
+    show(uni);
+});
+
+function show(uni){
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for (university of uni){
+        console.log(university.name);
+
+        let li = document.createElement('li');
+        li.innerText = university.name;
+        list.appendChild(li);  
     }
+}
 
+async function getUniversities(country){
+    try {
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        return [];
+    }
 }
