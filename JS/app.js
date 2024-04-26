14f59b8581c76b357c22271fea5fb7dd941537ed
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-26 11:29:02 +0600

    JS (first api request using async & await)

--- JS/app.js
+++ JS/app.js
@@ -15,20 +15,37 @@ let url = "https://catfact.ninja/fact";
 
 
 //now can access the api data
-fetch(url) //return promise
-.then((res)=>{
-    return res.json();
-})
-.then((data)=>{
-    console.log("Data-01: ", data.fact);
-    return fetch(url);//if we again get any api response
-})
-.then((res)=>{
-    return res.json();
-})
-.then((data)=>{
-    console.log("Data-02: ", data.fact)
-})
-.catch((err)=>{
-    console.log(`Error is: ${err}`);
-})
+// fetch(url) //return promise
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data)=>{
+//     console.log("Data-01: ", data.fact);
+//     return fetch(url);//if we again get any api response
+// })
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data)=>{
+//     console.log("Data-02: ", data.fact)
+// })
+// .catch((err)=>{
+//     console.log(`Error is: ${err}`);
+// })
+
+//async await 
+
+async function getFact(){
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log("Data-01: ", data.fact);
+
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log("Data-02: ", data2.fact);
+    } 
+    catch (error) {
+        console.log(`Error is: ${error}`);
+    }
+}
