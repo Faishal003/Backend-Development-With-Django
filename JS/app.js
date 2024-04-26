81da092a0930b27e63ffe83fcb6279b94d954ec9
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-26 12:40:42 +0600

    JS (another example of dog images using axios)

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,14 @@
 //fetch data
 
-let url = "https://catfact.ninja/fact";
+let url = "https://dog.ceo/api/breeds/image/random";
 
 let btn = document.querySelector("button");
-let p = document.querySelector("#result");
+let img = document.querySelector("#result");
 
 btn.addEventListener('click', async ()=>{
-    let fact = await getFacts();
-    console.log(fact);
-    p.innerText = fact; 
+    let link = await getFacts();
+    console.log(link);
+    img.setAttribute("src", link);
 })
 
 
@@ -17,10 +17,9 @@ async function getFacts(){
     try {
         let res = await axios.get(url);
         // console.log(res.data.fact);
-        
-        return res.data.fact;
+        return res.data.message;
     } catch (error) {
         console.log(`Error is: ${error}`);
-        return "No fact found"
+        return "No Image found";
     }
 }
