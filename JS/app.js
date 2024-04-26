96c2cd20dbdd729dbeefbe43986cdb48155dfeca
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-26 12:17:48 +0600

    JS (axios)

--- JS/app.js
+++ JS/app.js
@@ -1,51 +1,26 @@
 //fetch data
 
-
 let url = "https://catfact.ninja/fact";
 
+let btn = document.querySelector("button");
+let p = document.querySelector("#result");
 
-//get a readable steam
-// fetch(url) //return promise
-// .then((res)=>{
-//     console.log(res);
-// })
-// .catch((err)=>{
-//     console.log(`Error is: ${err}`);
-// })
+btn.addEventListener('click', async ()=>{
+    let fact = await getFacts();
+    console.log(fact);
+    p.innerText = fact; 
+})
 
 
-//now can access the api data
-// fetch(url) //return promise
-// .then((res)=>{
-//     return res.json();
-// })
-// .then((data)=>{
-//     console.log("Data-01: ", data.fact);
-//     return fetch(url);//if we again get any api response
-// })
-// .then((res)=>{
-//     return res.json();
-// })
-// .then((data)=>{
-//     console.log("Data-02: ", data.fact)
-// })
-// .catch((err)=>{
-//     console.log(`Error is: ${err}`);
-// })
-
-//async await 
-
-async function getFact(){
+//axios 
+async function getFacts(){
     try {
-        let res = await fetch(url);
-        let data = await res.json();
-        console.log("Data-01: ", data.fact);
-
-        let res2 = await fetch(url);
-        let data2 = await res2.json();
-        console.log("Data-02: ", data2.fact);
-    } 
-    catch (error) {
+        let res = await axios.get(url);
+        // console.log(res.data.fact);
+        
+        return res.data.fact;
+    } catch (error) {
         console.log(`Error is: ${error}`);
+        return "No fact found"
     }
 }
