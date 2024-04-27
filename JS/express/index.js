487e990c33f5be8ed3f3b9504636b3c84e5b10b5
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-27 17:09:59 +0600

    JS (routing)

--- JS/express/index.js
+++ JS/express/index.js
@@ -7,10 +7,32 @@ app.listen(port, ()=>{
     console.log(`app is listening on ${port}`);
 })
 
-app.use((req, res) =>{
-    console.log("request received");
-    res.send({
-        name: "Faishal ahmed emon",
-        age: 24,
-    })
+
+//app.use send same respone to all path
+// app.use((req, res) =>{
+//     console.log("request received");
+//     res.send({
+//         name: "Faishal ahmed emon",
+//         age: 24,
+//     })
+// })
+
+app.get('/', (req, res)=>{
+    res.send("You are in home page.")
+})
+
+app.get('/about', (req, res)=>{
+    res.send("You are in about page.")
+})
+
+app.get('/search', (req, res)=>{
+    res.send("You are in search page.")
+})
+
+app.get('*', (req, res)=>{
+    res.send("404 Bad request!!")
+})
+
+app.post('/', (req, res)=>{
+    res.send("You are in home page and request a post req")
 })
