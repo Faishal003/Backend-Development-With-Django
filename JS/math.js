2a79f4ba3fc483d930a91bb93a6c6f6cbb5b2695
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-27 07:31:43 +0600

    JS (module.exports)

---
+++ JS/math.js
@@ -0,0 +1,12 @@
+const sum = (a, b) => a + b;
+const mul = (a, b) => a * b;
+
+const g = 9.8;
+const pi = 3.1416;
+
+module.exports = {
+    sum: sum,
+    mul: mul,
+    g: g,
+    pi: pi
+};
---
+++ JS/script.js
@@ -0,0 +1,4 @@
+const math = require("./math");
+
+console.log(math.sum(2,3));
+console.log(math.mul(300,2));
